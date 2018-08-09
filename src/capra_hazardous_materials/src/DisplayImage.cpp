3f0341e7f976dc7b71be033290a5a26709ffb4d2
fredpaquette <frederic_paquette@hotmail.com>
2018-08-09 12:15:58 -0400

    add opencv ros dependancy

--- src/capra_hazardous_materials/src/DisplayImage.cpp
+++ src/capra_hazardous_materials/src/DisplayImage.cpp
@@ -1,8 +1,9 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <vector>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <string>
 
 #include "PolygonDetector.h"
 
@@ -10,27 +11,38 @@ using namespace cv;
 
 using namespace std;
 
-int main(int argc, char** argv )
+int main(int argc, char **argv)
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    PolygonDetector  detector = PolygonDetector(argv[1]);
-    
-    detector.detect();
-    
-   // Mat image;
-   // Mat hsv;
-   // Mat threshold;
-   // image = imread( argv[1], 1 );
 
-   //  cvtColor( image,  hsv, CV_BGR2HSV );
-  // inRange(hsv,Scalar(0,0,230),Scalar(255,230,255),threshold);
+    ros::init(argc, argv, "capra_hazardous_materials");
 
-   // namedWindow("Display Image", WINDOW_AUTOSIZE );
-   // imshow("Display Image",threshold);
-   // waitKey(0);
+    ros::NodeHandle n;
+
+   
+
+    ros::spin();
+    
     return 0;
+
+   // if (argc != 2)
+ //   {
+  //      printf("usage: DisplayImage.out <Image_Path>\n");
+   //     return -1;
+  //  }
+   // PolygonDetector detec
+   //     PolygonDetector detector = PolygonDetector(argv[1]);
+
+  //  detector.detect();
+
+    // Mat image;
+    // Mat hsv;
+    // Mat threshold;
+    // image = imread( argv[1], 1 );
+
+    //  cvtColor( image,  hsv, CV_BGR2HSV );
+    // inRange(hsv,Scalar(0,0,230),Scalar(255,230,255),threshold);
+
+    // namedWindow("Display Image", WINDOW_AUTOSIZE );
+    // imshow("Display Image",threshold);
+    // waitKey(0);
 }
